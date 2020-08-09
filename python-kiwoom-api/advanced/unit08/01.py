f98f0218a7cec9a5998edd4b7f2a5553438b492f
Brayden <brayden.jo@outlook.com>
2020-08-09 16:36:38 +0900

    변동성 돌파 전략 완성

--- python-kiwoom-api/advanced/unit08/01.py
+++ python-kiwoom-api/advanced/unit08/01.py
@@ -13,6 +13,9 @@ class MyWindow(QMainWindow):
 
         self.range = None
         self.target = None
+        self.account = None
+        self.amount = None
+        self.hold = None
 
         self.plain_text_edit = QPlainTextEdit(self)
         self.plain_text_edit.setReadOnly(True)
@@ -28,12 +31,21 @@ class MyWindow(QMainWindow):
     def CommConnect(self):
         self.ocx.dynamicCall("CommConnect()")
 
+    def GetLoginInfo(self, tag):
+        data = self.ocx.dynamicCall("GetLoginInfo(QString)", tag)
+        return data
+
     def _handler_login(self, err_code):
         if err_code == 0:
             self.plain_text_edit.appendPlainText("로그인 완료")
 
+            accounts = self.GetLoginInfo("ACCNO")
+            self.account = accounts.split(';')[0]
+
             # TR 요청 
             self.request_opt10081()
+            self.request_opw00001()
+
             # 주식체결 (실시간)
             self.subscribe_stock_conclusion('2')
 
@@ -45,6 +57,11 @@ class MyWindow(QMainWindow):
             self.range = int(고가) - int(저가)
             info = f"일자: {일자} 고가: {고가} 저가: {저가}"
             self.plain_text_edit.appendPlainText(info)
+        elif rqname == "예수금조회":
+            주문가능금액 = self.GetCommData(trcode, rqname, 0, "주문가능금액")
+            주문가능금액 = int(주문가능금액)
+            self.amount = int(주문가능금액 * 0.2)
+            self.plain_text_edit.appendPlainText(f"투자금액: {self.amount}")
 
     def request_opt10081(self):
         now = datetime.datetime.now()
@@ -53,6 +70,13 @@ class MyWindow(QMainWindow):
         self.SetInputValue("기준일자", today)
         self.SetInputValue("수정주가구분", 1)
         self.CommRqData("KODEX일봉데이터", "opt10081", 0, "9000")
+
+    def request_opw00001(self):
+        self.SetInputValue("계좌번호", self.account)
+        self.SetInputValue("비밀번호", "")
+        self.SetInputValue("비밀번호입력매체구분", "00")
+        self.SetInputValue("조회구분", 2)
+        self.CommRqData("예수금조회", "opw00001", 0, "9001")
 
     # 실시간 타입을 위한 메소드
     def SetRealReg(self, screen_no, code_list, fid_list, real_type):
@@ -71,7 +95,16 @@ class MyWindow(QMainWindow):
         if real_type == "주식체결":
             시가 = self.GetCommRealData(code, 16)
             시가 = abs(int(시가))          # +100, -100
+            현재가 = self.GetCommRealData(code, 10)
+            현재가 = abs(int(현재가))          # +100, -100
             self.target = int(시가 + (self.range) * 0.5)      
+            self.plain_text_edit.appendPlainText(f"목표가: {self.target}")
+
+            # 매수 
+            if self.hold is None and 현재가 > self.target:
+                self.hold = True 
+                quantity = int(self.amount / 현재가)
+                self.SendOrder("매수", "8000", self.account, 1, "229200", quantity, 0, "03", "")
 
     def subscribe_stock_conclusion(self, screen_no):
         self.SetRealReg(screen_no, "229200", "20", 0)
@@ -89,6 +122,10 @@ class MyWindow(QMainWindow):
         data = self.ocx.dynamicCall("GetCommData(QString, QString, int, QString)", 
                                      trcode, rqname, index, item)
         return data.strip()
+
+    def SendOrder(self, rqname, screen, accno, order_type, code, quantity, price, hoga, order_no):
+        self.ocx.dynamicCall("SendOrder(QString, QString, QString, int, QString, int, int, QString, QString)",
+                             [rqname, screen, accno, order_type, code, quantity, price, hoga, order_no])
 
 
 if __name__ == "__main__":
--- python-kiwoom-api/advanced/unit08/02.py
+++ python-kiwoom-api/advanced/unit08/02.py
@@ -13,6 +13,12 @@ class MyWindow(QMainWindow):
 
         self.range = None
         self.target = None
+        self.account = None
+        self.amount = None
+        self.hold = None
+
+        self.previous_day_hold = False
+        self.previous_day_quantity = False
 
         self.plain_text_edit = QPlainTextEdit(self)
         self.plain_text_edit.setReadOnly(True)
@@ -28,13 +34,25 @@ class MyWindow(QMainWindow):
     def CommConnect(self):
         self.ocx.dynamicCall("CommConnect()")
 
+    def GetLoginInfo(self, tag):
+        data = self.ocx.dynamicCall("GetLoginInfo(QString)", tag)
+        return data
+
     def _handler_login(self, err_code):
         if err_code == 0:
             self.plain_text_edit.appendPlainText("로그인 완료")
 
+            accounts = self.GetLoginInfo("ACCNO")
+            self.account = accounts.split(';')[0]
+            print(self.account)
+
             # TR 요청 
             self.request_opt10081()
+            self.request_opw00001()
+            self.request_opw00004()
+
             # 주식체결 (실시간)
+            self.subscribe_market_time('1')
             self.subscribe_stock_conclusion('2')
 
     def _handler_tr_data(self, screen_no, rqname, trcode, record, next):
@@ -46,6 +64,26 @@ class MyWindow(QMainWindow):
             info = f"일자: {일자} 고가: {고가} 저가: {저가}"
             self.plain_text_edit.appendPlainText(info)
 
+        elif rqname == "예수금조회":
+            주문가능금액 = self.GetCommData(trcode, rqname, 0, "주문가능금액")
+            주문가능금액 = int(주문가능금액)
+            self.amount = int(주문가능금액 * 0.2)
+            self.plain_text_edit.appendPlainText(f"투자금액: {self.amount}")
+
+        elif rqname == "계좌평가현황":
+            rows = self.GetRepeatCnt(trcode, rqname)
+            for i in range(rows):
+                종목코드 = self.GetCommData(trcode, rqname, i, "종목코드")
+                보유수량 = self.GetCommData(trcode, rqname, i, "보유수량")
+
+                if 종목코드[1:] == "229200":
+                    self.previous_day_hold = True
+                    self.previous_day_quantity = int(보유수량)
+
+    def GetRepeatCnt(self, trcode, rqname):
+        ret = self.ocx.dynamicCall("GetRepeatCnt(QString, QString)", trcode, rqname)
+        return ret
+
     def request_opt10081(self):
         now = datetime.datetime.now()
         today = now.strftime("%Y%m%d")
@@ -53,6 +91,20 @@ class MyWindow(QMainWindow):
         self.SetInputValue("기준일자", today)
         self.SetInputValue("수정주가구분", 1)
         self.CommRqData("KODEX일봉데이터", "opt10081", 0, "9000")
+
+    def request_opw00001(self):
+        self.SetInputValue("계좌번호", self.account)
+        self.SetInputValue("비밀번호", "")
+        self.SetInputValue("비밀번호입력매체구분", "00")
+        self.SetInputValue("조회구분", 2)
+        self.CommRqData("예수금조회", "opw00001", 0, "9001")
+    
+    def request_opw00004(self):
+        self.SetInputValue("계좌번호", self.account)
+        self.SetInputValue("비밀번호", "")
+        self.SetInputValue("상장폐지조회구분", 0)
+        self.SetInputValue("비밀번호입력매체구분", "00")
+        self.CommRqData("계좌평가현황", "opw00004", 0, "9002")
 
     # 실시간 타입을 위한 메소드
     def SetRealReg(self, screen_no, code_list, fid_list, real_type):
@@ -68,14 +120,35 @@ class MyWindow(QMainWindow):
 
     # 실시간 이벤트 처리 핸들러 
     def _handler_real_data(self, code, real_type, real_data):
-        if real_type == "주식체결":
+        if real_type == "장시작시간":
+            장운영구분 = self.GetCommRealData(code, 215)
+            if 장운영구분 == '3':
+                if self.previous_day_hold:
+                    self.previous_day_hold = False
+                    # 매도 (시장가)
+                    self.SendOrder("매도", "8001", self.account, 2, "229200", self.previous_day_quantity, 0, "03", "")
+
+        elif real_type == "주식체결":
             시가 = self.GetCommRealData(code, 16)
             시가 = abs(int(시가))          # +100, -100
+            현재가 = self.GetCommRealData(code, 10)
+            현재가 = abs(int(현재가))          # +100, -100
             self.target = int(시가 + (self.range) * 0.5)      
+            self.plain_text_edit.appendPlainText(f"목표가: {self.target}")
+
+            # 매수 
+            if self.hold is None and 현재가 > self.target:
+                self.hold = True 
+                quantity = int(self.amount / 현재가)
+                self.SendOrder("매수", "8000", self.account, 1, "229200", quantity, 0, "03", "")
 
     def subscribe_stock_conclusion(self, screen_no):
         self.SetRealReg(screen_no, "229200", "20", 0)
         self.plain_text_edit.appendPlainText("주식체결 구독신청")
+
+    def subscribe_market_time(self, screen_no):
+        self.SetRealReg(screen_no, "", "215", 0)
+        self.plain_text_edit.appendPlainText("장시작시간 구독신청")
 
     # TR 요청을 위한 메소드
     def SetInputValue(self, id, value):
@@ -89,6 +162,10 @@ class MyWindow(QMainWindow):
         data = self.ocx.dynamicCall("GetCommData(QString, QString, int, QString)", 
                                      trcode, rqname, index, item)
         return data.strip()
+
+    def SendOrder(self, rqname, screen, accno, order_type, code, quantity, price, hoga, order_no):
+        self.ocx.dynamicCall("SendOrder(QString, QString, QString, int, QString, int, int, QString, QString)",
+                             [rqname, screen, accno, order_type, code, quantity, price, hoga, order_no])
 
 
 if __name__ == "__main__":
